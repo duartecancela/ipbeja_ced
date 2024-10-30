0525ca5d7a13158a68ce1b5f201fb2f08518eda4
ivovargas <91195754@stu.ipbeja.pt>
2024-10-30 22:48:57 +0000

    OMP

---
+++ aula3/integral_a.c
@@ -0,0 +1,66 @@
+/*
+HPC
+aula3.c
+Ivo Vargas
+2024-10-30
+
+Compilar:
+nvc++ aula3.c -fopenmp -o aula3
+
+Exemplo de integral para calcular areas
+*/
+#include <stdio.h>
+#include <math.h>
+#include <omp.h>
+#include <unistd.h>
+
+// Função que devolve uma constante
+double f1(double x) {
+    return 1.0;
+}
+
+double f2(double x) {
+    return x*x;
+}
+
+double f_pi(double x) {
+    return sqrt(1-x*x);
+}
+
+double soma_integral(double a, double b, int N,double (*f)(double)) {
+    double dx = (b-a)/N;
+    double s = 0;
+
+    for (int k = 0; k < N; k++) {
+        s += f(a+k*dx)*dx;
+    }
+    return s;
+}
+
+int main() {
+    double a = 0.0;
+    double b = 1.0;
+    int N = 5000000;
+
+    double s = soma_integral(a, b, N, f_pi);
+    double pi = 4*s;
+
+    printf("Pi Calculado = %f\n", pi);
+
+    /*for (int k = 0; k < 10; k++)
+    {
+        printf("k = %d\n", k);
+    }*/
+
+#pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        for (int i = 0; i < 100; i++)
+        {
+            double s = soma_integral(a, b, N, f_pi);
+        }
+        printf("thread_id = %d\n", thread_id);
+    }
+
+    return 0;
+}
