015fe27e5278a30d90d4aea71bf34105dd5b0397
Pedro Miranda <pmargalha@gmail.com>
2024-11-01 19:59:52 +0000

    Limpeza

---
+++ aula2/hello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// compilar:
+//    nvcc hello.c -o hello.o
+
+int main(int argc, char *argv[])
+{
+    printf("hello world\n");
+    return EXIT_SUCCESS;
+}
--- aula3/integral_a.c
+++ aula3/integral_a.c
@@ -5,45 +5,51 @@ Ivo Vargas
 2024-10-30
 
 Compilar:
-nvc++ aula3.c -fopenmp -o aula3
+nvc++ integral_a.c -fopenmp -o integral_c.o
 
 Exemplo de integral para calcular areas
 */
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
-#include <unistd.h>
+// #include <unistd.h> biblioteca da função sleep()
 
 // Função que devolve uma constante
-double f1(double x) {
+double f1(double x)
+{
     return 1.0;
 }
 
-double f2(double x) {
-    return x*x;
+double f2(double x)
+{
+    return x * x;
 }
 
-double f_pi(double x) {
-    return sqrt(1-x*x);
+double f_pi(double x)
+{
+    return sqrt(1 - x * x);
 }
 
-double soma_integral(double a, double b, int N,double (*f)(double)) {
-    double dx = (b-a)/N;
+double soma_integral(double a, double b, int N, double (*f)(double))
+{
+    double dx = (b - a) / N;
     double s = 0;
 
-    for (int k = 0; k < N; k++) {
-        s += f(a+k*dx)*dx;
+    for (int k = 0; k < N; k++)
+    {
+        s += f(a + k * dx) * dx;
     }
     return s;
 }
 
-int main() {
+int main()
+{
     double a = 0.0;
     double b = 1.0;
     int N = 5000000;
 
     double s = soma_integral(a, b, N, f_pi);
-    double pi = 4*s;
+    double pi = 4 * s;
 
     printf("Pi Calculado = %f\n", pi);
 
