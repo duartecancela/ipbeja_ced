44cfed8782450ed02e15bbc90a05c61a33d2b999
Pedro Miranda <pmargalha@gmail.com>
2024-11-09 19:26:13 +0000

    Reorganiza código da aula 3 e adiciona makefile

--- aula3/integral_a.cpp
+++ aula3/integral_a.cpp
@@ -1,74 +1,60 @@
 /*
 HPC
-aula3.c
-Ivo Vargas
+aula3.cpp
+Pedro Miranda
 2024-10-30
 
-Compilar:
-nvc++ integral_a.cpp -fopenmp -o integral_a.o
-ou
-g++ integral_a.cpp -fopenmp -o integral_a.o
+Um exemplo de soma integral para cálculo de áreas
 
-Exemplo de integral para calcular areas
+Compilar: 
+	nvc++ aula3.cpp -fopenmp -o aula3
 */
-#include <stdio.h>
-#include <math.h>
+
+#include <iostream>
 #include <omp.h>
-// #include <unistd.h> biblioteca da função sleep()
+//#include <unistd.h>
+#include "f.h"
 
-// Função que devolve uma constante
-double f1(double x)
+double soma_integral(double a, double b, int N, 
+					 double (*f)(double))
 {
-    return 1.0;
+	double s = 0.0;
+	double dx = (b-a) / N;
+
+	for(int k = 0; k < N; k++){
+		s += f(a+k*dx) * dx;
+	}
+	return s;
 }
 
-double f2(double x)
-{
-    return x * x;
-}
+int main() {
+	std::cout << "Hello World!"
+	<< std::endl;
 
-double f_pi(double x)
-{
-    return sqrt(1 - x * x);
-}
+	double a = 0;
+	double b = 1.0;
 
-double soma_integral(double a, double b, int N, double (*f)(double))
-{
-    double dx = (b - a) / N;
-    double s = 0;
+	int N = 5000000;
 
-    for (int k = 0; k < N; k++)
-    {
-        s += f(a + k * dx) * dx;
-    }
-    return s;
-}
+	double s = soma_integral(a,b,N, f_pi);
 
-int main()
-{
-    double a = 0.0;
-    double b = 1.0;
-    int N = 5000000;
+	double pi = 4 * s;
 
-    double s = soma_integral(a, b, N, f_pi);
-    double pi = 4 * s;
+	std::cout << "soma = " << s << std::endl;
+    std::cout << "pi calculado = " << pi << std::endl;
 
-    printf("Pi Calculado = %f\n", pi);
+	// for(int k = 0; k < 10; k++){
+	//	std::cout << k << std::endl;
+	//}
 
-    /*for (int k = 0; k < 10; k++)
-    {
-        printf("k = %d\n", k);
-    }*/
-
-#pragma omp parallel
-    {
-        int thread_id = omp_get_thread_num();
-        for (int i = 0; i < 100; i++)
-        {
-            double s = soma_integral(a, b, N, f_pi);
-        }
-        printf("thread_id = %d\n", thread_id);
-    }
-
-    return 0;
+	#pragma omp parallel
+	{
+		int thread_id = omp_get_thread_num();
+		//sleep(1);
+		for(int n = 0; n<5; n++)
+			double s = soma_integral(a,b,N, f_pi);
+		std::cout << thread_id << std::endl;
+	}
+    	
+	return 0;
 }
